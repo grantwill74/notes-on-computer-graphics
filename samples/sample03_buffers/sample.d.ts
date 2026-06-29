d60560d82e97e94742d257ecbf8b66b9edd6ebf4
Grant <22186487+grantwill74@users.noreply.github.com>
2026-06-29 16:22:43 -0700

    move buffer example to sample03

---
+++ samples/sample03_buffers/sample.d.ts
@@ -0,0 +1,3 @@
+export declare function createFloat32Buffer(device: GPUDevice, data: Float32Array, usage: number, label?: string | undefined): GPUBuffer;
+export declare function renderSample03_vertsInBuffer(device: GPUDevice, context: GPUCanvasContext): void;
+//# sourceMappingURL=sample.d.ts.map
